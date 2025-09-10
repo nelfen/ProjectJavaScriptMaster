8683bded5d094edbec782a1343864c95be4ae33c
nelfen <nelfen.dev@gmail.com>
2025-09-10 18:11:55 +0900

    수정본

---
+++ JavsScript/oz/Daily/script.js
@@ -0,0 +1,42 @@
+const buttons = document.querySelectorAll(".button");
+const display = document.querySelector(".display p");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const buttonValue = button.textContent;
+    const currentDisplayValue = display.textContent;
+
+    switch (buttonValue) {
+      case 'C':
+          display.textContent = '0';
+          break;
+      case '.':
+          if (!currentDisplayValue.includes('.')) {
+            display.textContent += '.';
+          }
+          break;
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+
+        if (currentDisplayValue === '0') {
+          display.textContent = buttonValue;
+        } else {
+          display.textContent += buttonValue;
+        }
+        break;
+
+      default:
+        console.log(buttonValue);
+        break;
+
+    }
+  });
+});
